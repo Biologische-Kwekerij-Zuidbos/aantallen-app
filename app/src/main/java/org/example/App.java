f9ba87e0ac7d6de72ce01027c3c6c02e3bc2b805
hansAGB <hans.aangeenbrug@icloud.com>
2024-02-21 20:04:56 +0100

    Add initial View

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -3,8 +3,6 @@
  */
 package org.example;
 
-import org.example.view.SomeView;
-
 import javafx.application.Application;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -13,13 +11,16 @@ import javafx.stage.Stage;
 // See https://fxdocs.github.io/docs/html5/#_the_mvvm_pattern
 public class App extends Application {
 
-    private Parent createContent() {
-        return new SomeView();
+    private static final String WINDOW_TITLE = "Aantallen verwerken";
+
+    private Parent createContent(final Stage stage) {
+        return new View(stage);
     }
 
     @Override
     public void start(Stage stage) throws Exception {
-        stage.setScene(new Scene(createContent(), 300, 300));
+        stage.setScene(new Scene(createContent(stage), 300, 200));
+        stage.setTitle(WINDOW_TITLE);
         stage.show();
     }
 
---
+++ app/src/main/java/org/example/View.java
@@ -0,0 +1,88 @@
+package org.example;
+
+import org.example.model.SomeViewModel;
+
+import javafx.event.ActionEvent;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.Separator;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+public class View extends VBox {
+    private GridPane gp = new GridPane();
+
+    private FileChooser fileChooser = new FileChooser();
+    private Button fileSelectButton = new Button("Selecteer..");
+    private DatePicker datePicker = new DatePicker();
+
+    private Button printButton = new Button("Print");
+
+    public View(final Stage stage) {
+        createView(stage);
+        bindViewModel();
+    }
+
+    private final SomeViewModel viewModel = new SomeViewModel();
+
+    private void bindViewModel() {
+        // fileSelectButton.onCLick
+
+        // Bindings.bindBidirectional(
+        //         tfAnnualSalary.textProperty(),
+        //         viewModel.annualSalaryProperty(),
+        //         new NumberStringConverter());
+    }
+
+    private void createView(final Stage stage) {
+        VBox gpwrap = new VBox();
+        gpwrap.setAlignment(Pos.CENTER);
+
+        gp.setPadding(new Insets(40));
+        gp.setVgap(4);
+        gp.add(fileSelectButton, 0, 0);
+        gp.add(new Label("Datum:"), 0, 1);
+        gp.add(datePicker, 1, 1);
+
+        final ColumnConstraints col = new ColumnConstraints();
+        col.setPercentWidth(50);
+
+        gp.getColumnConstraints().addAll(col, col);
+
+        gpwrap.getChildren().add(gp);
+
+        VBox.setVgrow(gpwrap, Priority.ALWAYS);
+
+        printButton.setOnAction(this::save);
+        fileSelectButton.setOnAction(event -> this.chooseFile(event, stage));
+
+        printButton.setDefaultButton(true);
+
+        ButtonBar buttonBar = new ButtonBar();
+        buttonBar.setPadding(new Insets(20.0d));
+        ButtonBar.setButtonData(printButton, ButtonBar.ButtonData.OK_DONE);
+
+        buttonBar.getButtons().addAll(printButton);
+
+        this.getChildren().addAll(
+                gpwrap,
+                new Separator(),
+                buttonBar);
+    }
+
+    private void save(ActionEvent evt) {
+        viewModel.save();
+    }
+
+    private void chooseFile(ActionEvent event, final Stage stage) {
+        fileChooser.showOpenDialog(stage);
+    }
+}
