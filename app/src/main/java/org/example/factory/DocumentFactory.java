483cc9f4e8296cdb55975a382a409abd05c38864
hansAGB <hans.aangeenbrug@icloud.com>
2024-03-06 18:43:04 +0100

    Implement DocumentFactory

--- app/src/main/java/org/example/factory/DocumentFactory.java
+++ app/src/main/java/org/example/factory/DocumentFactory.java
@@ -1,14 +1,32 @@
 package org.example.factory;
 
 import java.io.File;
+import java.io.IOException;
 import java.time.LocalDate;
+import java.util.Map;
+import java.util.Set;
+
+import org.example.builder.DocumentBuilder;
+import org.example.reader.DocumentReader;
 
 public class DocumentFactory implements IDocumentFactory {
 
+    private final DocumentReader reader = new DocumentReader();
+
     @Override
-    public File createFromDate(LocalDate date) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'createFromDate'");
+    public File createFromDate(LocalDate date) throws IOException {
+        DocumentBuilder builder = new DocumentBuilder();
+        Set<String> absentPeopleNames = reader.readAbsentPeopleNames(date);
+        Set<String> presentPeopleAfterAbsenceNames = reader.readPresentPeopleAfterAbsenceNames(date);
+        int totalPackagesInTwos = reader.readTotalPackagesInTwos(date);
+        Map<Integer, Integer> totalPeoplePerPackageSize = reader.readTotalPeoplePerPackageSize(date);
+
+        return builder
+                .withAbsentPeople(absentPeopleNames)
+                .withPresentPeopleAfterAbsence(presentPeopleAfterAbsenceNames)
+                .withTotalPackagesInTwos(totalPackagesInTwos)
+                .withTotalPeoplePerPackageSize(totalPeoplePerPackageSize)
+                .build();
     }
 
 }
--- app/src/main/java/org/example/factory/IDocumentFactory.java
+++ app/src/main/java/org/example/factory/IDocumentFactory.java
@@ -1,10 +1,11 @@
 package org.example.factory;
 
 import java.io.File;
+import java.io.IOException;
 import java.time.LocalDate;
 
 public interface IDocumentFactory {
     
-    public File createFromDate(LocalDate date);
+    public File createFromDate(LocalDate date) throws IOException;
 
 }
